6fbd5f0975ae994d53dbaf957426f4ebd6300f59
HuguitoS2r <h.huichalaogarcia@gmail.com>
2024-09-14 23:30:14 -0300

    persistence and improvements

---
+++ src/app/shared/components/loadingSpinner/loadingSpinner.component.ts
@@ -0,0 +1,8 @@
+import {  Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-loading-spinner', 
+  templateUrl: './loadingSpinner.component.html',
+  styleUrls: ['./loading-spinner.component.css']
+})
+export class LoadingSpinnerComponent { }
--- src/app/shared/components/searchBox/searchBox.component.ts
+++ src/app/shared/components/searchBox/searchBox.component.ts
@@ -1,4 +1,5 @@
-import {  Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import {  Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
@@ -10,15 +11,43 @@ import {  Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '
     }
   `,
 })
-export class SearchBoxComponent {   
+export class SearchBoxComponent implements OnInit, OnDestroy{
+
+
+  private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSuscription?: Subscription;
+
+  ngOnDestroy(): void {
+    this.debouncerSuscription?.unsubscribe();
+  }
+
+  ngOnInit(): void {
+     this.debouncerSuscription = this.debouncer
+    .pipe(
+      debounceTime(300)
+    )
+    .subscribe(value => {
+      this.onDebounce.emit(value);
+    });
+  }   
 
   @Output()
   public onValue = new EventEmitter<string>();
 
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
   @Input()
   public placeholder: string = '';
 
+  @Input()
+  public initialValue: string = '';
+
   emitValue(value:string):void {
     this.onValue.emit(value);
+  }
+
+  onKeyPress(searchTerm: string):void {
+    this.debouncer.next(searchTerm);
   }
 }
